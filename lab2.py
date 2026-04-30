4d458c2972e32e67c4589d211119c394af5e0050
Hardik Gupta <socialguptaji29@gmail.com>
2026-04-30 21:29:51 +0530

    Add files via upload

--- lab2.py
+++ lab2.py
@@ -1,17 +1,41 @@
-print(" Welcome to Mustafa central shopping mall ")
-discount =0
-total=0
-items=int(input("Enter no of items purchased"))
-for i in range(1,items+1):
-    price= float(input(f"Enter price of item{i} - "))
-    print(f"prince of item {i} : Rs {price}")
-    total =total + price
-if(total>100):
-    discount=total*0.1
-print("================================")
-print(" Final bill ")
-print("Total cost of items - Rs ", total)
-print("Discount applied - Rs ",discount)
-print(" The customer has to pay Rs - " ,total-discount)
-print("=========================")
-print("Happy shopping")
+def icecreamparlour():
+
+    menu = {
+        "chocolate": 50,
+        "vanilla": 45,
+        "strawberry": 70,
+        "mango": 80
+    }
+
+    print("WELCOME TO ICECREAM WORLD")
+    print("========= MENU ==========")
+
+    for item, price in menu.items():
+        print(f"{item.capitalize()} - Rs {price}")
+
+    total = 0
+
+    while True:
+        choice = input("Enter the flavour you want: ").lower()
+
+        if choice in menu:
+            total += menu[choice]
+            print(f"{choice.capitalize()} added to your order")
+        else:
+            print("Invalid choice")
+
+        more = input("Do you want to order more (yes/no): ").lower()
+        if more != "yes":
+            break
+
+    tax = total * 0.08
+    final_bill = total + tax
+
+    print("\n\nFINAL BILL\n")
+    print(f"Total cost of items: Rs {total}")
+    print(f"Tax applied: Rs {tax:.2f}")
+    print(f"\nFinal amount to be paid: Rs {final_bill:.2f}")
+
+
+# Calling the function
+icecreamparlour()
